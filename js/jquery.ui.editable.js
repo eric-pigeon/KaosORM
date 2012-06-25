59af3b4e5a4733f2073716f70b95c690c43063c2
Eric Pigeon <eric.r.pigeon@gmail.com>
2012-06-25 08:08:40 -0400

    Added edit callback. Added placeholder for validate callback in options.

--- js/jquery.ui.editable.js
+++ js/jquery.ui.editable.js
@@ -3,13 +3,16 @@
 (function ( $ ) {
 
 $.widget('kaosORM.editable', {
-	options : { },
+	options : {
+		validate: null,
+		edit: null	
+	},
 	_create : function() {
 		this.element.addClass('editable');
 		var self = this;
 		this.element.bind('dblclick.editable', function() {
 			var $this = $(this);
-			self.oldText = $this.html();
+			self.oldValue = $this.html();
 			$this.html('');
 			self._createInput();
 		});
@@ -19,7 +22,7 @@ $.widget('kaosORM.editable', {
 	},
 	_createInput : function() {
 		var self = this;
-		var input = $('<input type="text">').val( this.oldText ).addClass('editableInput');
+		var input = $('<input type="text">').val( this.oldValue ).addClass('editableInput');
 		input.css({
 			'text-align' : this.element.css('text-align')
 		}).keydown(function (e) {
@@ -35,12 +38,29 @@ $.widget('kaosORM.editable', {
 	},
 	_endEdit : function() {
 		var newValue = this.element.children('input').val();
+		if ( typeof this.options.validationFunction == 'function') {
+			if (this.options.validationFunction(newValue)) {
+				this._doEdit();
+			} else {
+				this._cancelEdit();
+			}
+		} else {
+			this._doEdit();
+		}
+	},
+	_doEdit : function() {
+		var newValue = this.element.children('input').val();
 		this.element.children('input').remove();
 		this.element.html( newValue );
+		this._trigger('edit', null, {
+			uiObject : this,
+			oldValue : this.oldValue,
+			newValue : newValue
+		});
 	},
 	_cancelEdit : function() {
 		this.element.children('input').remove();
-		this.element.html( this.oldText );
+		this.element.html( this.oldValue );
 	}
 });
 
