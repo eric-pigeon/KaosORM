04974599e03b1203c3568d39e09b372106599972
Eric Pigeon <eric.r.pigeon@gmail.com>
2012-06-27 23:42:06 -0400

    Changed default action to trigger on keydown instead of keyup to prevent picking up improper actions.

--- js/jquery.defaultAction.js
+++ js/jquery.defaultAction.js
@@ -1,5 +1,6 @@
 (function ($) {
-	$(document).bind('keyup', function(e) {
+	$(document).on('keydown', function(e) {
+		e.stopImmediatePropagation();
 		if (e.keyCode == 13 && $('#defaultAction').length)
 			$('#defaultAction').click();
 	});
