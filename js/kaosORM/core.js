7417f8bd2044c467cf41ac66b68b01ff30c8875f
Eric Pigeon <eric.r.pigeon@gmail.com>
2012-07-04 13:16:24 -0400

    Made the KaosORM object a simple object.  Added some tab functionality. Added some selection functionality.

--- js/kaosORM/core.js
+++ js/kaosORM/core.js
@@ -3,41 +3,49 @@
 var KaosORM;
 
 (function($) {
-	KaosORM = function() {
-		if (this.instance != null)
-			return this.instance;
-		KaosORM.prototype.instance = this;
-		
-		this.dataModels = { };
-		this.dataModels.untitled = new KaosORM.DataModel();
-	};
 
-	KaosORM.prototype = {
-		instance : null,
-		dialogError : function( message ) {
-			$('<div>'+message+'</div>').attr('title', 'Error').dialog({
-				modal : true,
-				closeText : "",
- 				buttons : [
-					{
-						text : "Okay",
-						id : "defaultAction",
-						click : function() {
-							$(this).dialog("close");
-						}
+	KaosORM = {};
+	KaosORM.dataModels = [];
+	KaosORM.activeDataModel;
+
+	KaosORM.dialogError = function( message ) {
+		$('<div>'+message+'</div>').attr('title', 'Error').dialog({
+			modal : true,
+			closeText : "",
+ 			buttons : [
+				{
+					text : "Okay",
+					id : "defaultAction",
+					click : function() {
+						$(this).dialog("close");
 					}
-				],
-				close : function( event, ui ) {
-					$(event.target).dialog('destroy').remove();
 				}
-			});
-		}
+			],
+			close : function( event, ui ) {
+				$(event.target).dialog('destroy').remove();
+			}
+		});
 	};
 
+	KaosORM.dialog = function( message ) {
+
+	};
+})(jQuery);
+
+(function($) {
+	$(document).on('newSelection', function(e) {
+		$('#addPropertyButton').removeAttr('disabled');
+	});
+
+	$('#fileTabList').on('click', '.fileTab', function() {
+		$(this).addClass('active');
+	});
 })(jQuery);
 
 $(function() {
-	var kaosORM = new KaosORM();
+	KaosORM.activeDataModel = new KaosORM.DataModel();
+	KaosORM.dataModels.push(KaosORM.activeDataModel); 
+	$('#fileTabList').children(':first').addClass('active');
 
 	// this kinda works but needs to be fixed and cleaned up
 	$('#rightSideBarSepperator').mousedown(function(e) {
@@ -54,9 +62,18 @@ $(function() {
 	});
 
 	$('#addEntityButton').click(function() {
-		new KaosORM.Entity();
+		KaosORM.activeDataModel.addEntity();
 	});
 
-	console.log(kaosORM);
+	$('#addPropertyButton').click(function() {
+		// props want to make a dialog here to prompt for type
+		KaosORM.activeDataModel.gui.currentSelectedObject.data('entity').addProperty();
+	});
+
+	$('#addTabButton').click(function() {
+		new KaosORM.DataModel();
+	});
+
+	$('#addPropertyButton').attr('disabled', 'disabled');
 
 });
