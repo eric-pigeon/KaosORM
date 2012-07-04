7f433c9089d5d97003aa778ebf48b2ee9da31de9
Eric Pigeon <eric.r.pigeon@gmail.com>
2012-07-04 15:19:06 -0400

    Started to create the data model object with basic functionality.

--- js/kaosORM/dataModel.js
+++ js/kaosORM/dataModel.js
@@ -2,9 +2,27 @@
 
 var DataModel = function() {
 	this.entities = { };
+	this.gui = {
+		currentSelectedObject : null
+	};
+	this.element = $('<li class="fileTab">Untitled</li>');
+	this.element.appendTo( $('#fileTabList') ).addClass('transitioned');
 };
 
-DataModel.prototype = { };
+DataModel.prototype = {
+	addEntity : function() {
+		var i = 0;
+		var entityName = "Entity";
+		while(1) {
+			if (this.entities[entityName] === undefined) {
+				break;
+			}
+			i++;
+			entityName = "Entity"+i;
+		}
+		this.entities[entityName] = new KaosORM.Entity(entityName);
+	}
+};
 
 KaosORM.DataModel = DataModel;
 
